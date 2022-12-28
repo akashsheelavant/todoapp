4c3e3d780b72f78e6c4daaf17d1d3d2b47b0cfa7
Akash <akashsheelavant4@gmail.com>
2022-12-28 19:05:59 +0530

    Using a lifecycle method to get data using axios.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Home extends Component {
 
-    render() {
-        return (
-            <div className="container">
-                <h4 className="center">Home</h4>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt perspiciatis illum vitae in possimus, tenetur minima voluptates commodi laudantium itaque ut doloribus illo officiis nulla vero magni perferendis facilis voluptas!</p>            
-            </div>
-        );
-    }
+state = {
+    posts: []
+}
+
+componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(result => {
+        this.setState({
+            posts: result.data.slice(0, 10)            
+        })        
+    })
+}
+
+render() {
+    const { posts } = this.state
+    const postList = posts.length ? (
+        posts.map(post => {
+            return (
+                <div className="post card" key={post.id}>
+                    <div className="card-content">
+                        <span className="card-title">{post.title}</span>
+                        <p>{post.body}</p>
+                    </div>
+                </div>
+            )
+        })
+    ) : (
+        <div className="center">No posts yet</div>
+    ) 
+
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            {postList}
+        </div>
+    );
+}
 }
  
 export default Home
