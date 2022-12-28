f14c086b2252ae62884f2933bf0545f9189eceb3
Akash <akashsheelavant4@gmail.com>
2022-12-28 18:38:40 +0530

    Making Home a class component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Home = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt perspiciatis illum vitae in possimus, tenetur minima voluptates commodi laudantium itaque ut doloribus illo officiis nulla vero magni perferendis facilis voluptas!</p>            
-        </div>
-    );
+class Home extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt perspiciatis illum vitae in possimus, tenetur minima voluptates commodi laudantium itaque ut doloribus illo officiis nulla vero magni perferendis facilis voluptas!</p>            
+            </div>
+        );
+    }
 }
  
 export default Home
